235463729e10f398e8d8c04d59c1a9bf685fe84a
Kevin Printz <kevinprintz@microsoft.com>
2022-10-05 09:28:48 -0400

    fix

--- vm-windows/resources.tf
+++ vm-windows/resources.tf
@@ -13,7 +13,7 @@ resource "azurerm_network_interface" "nic" {
 
 # Create a VM
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "vm-linux-${var.project_name}"
+  name                = "vm-windows-${var.project_name}"
   resource_group_name = var.rg_name
   location            = var.location
   size                = "Standard_D2ds_v4"
